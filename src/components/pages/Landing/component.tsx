77469361041f18b84305d5c7410db2259130656d
SamuelRCrider <sam.crider@aol.com>
2024-07-01 19:48:00 -0400

    signed out landing page

--- src/components/pages/Landing/component.tsx
+++ src/components/pages/Landing/component.tsx
@@ -1,13 +1,13 @@
 import Navbar from "@/components/base/NavBar/component";
 import Feed from "@/components/compound/Feed/component";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import React from "react";
 
 const LandingPage = () => {
   return (
     <div className="flex flex-col h-full w-full p-6">
       <Navbar />
-      <div className="flex flex-row h-full justify-evenly items-center">
+      <div className="flex flex-row h-full justify-around items-center">
         <Feed />
         <SignedIn>
           <div>
@@ -15,7 +15,20 @@ const LandingPage = () => {
           </div>
         </SignedIn>
         <SignedOut>
-          <div>sign in to create your own art</div>
+          <div className="card bg-base-100 w-96 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img src="favicon.ico" alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">You aren't signed in</h2>
+              <p>Want to join the fun?</p>
+              <div className="card-actions">
+                <div className="btn btn-secondary">
+                  <SignInButton />
+                </div>
+              </div>
+            </div>
+          </div>
         </SignedOut>
       </div>
     </div>
